091adafe4ab17719c07e437632fba9a553d793c5
TheZetner <adrian.zetner@gmail.com>
2019-05-16 11:18:02 -0500

    Update get-dplyr-files.R

--- get-dplyr-files.R
+++ get-dplyr-files.R
@@ -1,9 +1,9 @@
 # Get Data
-# File>Import Dataset>From Excel...
-# Navigate to /drives/w/Temporary/Adrian/20190516-dplyr
-# Import those files
 
-# identify the folders
+# Navigate to /drives/w/Temporary/Adrian/20190516-dplyr and get exercises
+# Don't do: Mkdirs data and R
+
+# Identify the folders
 current.folder <- "/Drives/W/Temporary/Adrian/20190516-dplyr/"
 new.folder <- "./"
 
@@ -13,10 +13,9 @@ list.of.files <- list.files(current.folder, full.names = T)
 # copy the files to the new folder
 file.copy(list.of.files, new.folder)
 
-# Import files
-BC_Postal_Codes <- read_excel("BC_Postal_Codes.xlsx")
-Complete_Table <- read_excel("Complete_Table.xlsx")
-HealthWeek_LookupTbl <- read_excel("HealthWeek_LookupTbl_2019May.xlsx")
-ToFillIn <- read_excel("ToFillIn.xlsx")
-WGS_metadata <- read_excel("WGS_metadata.xlsx")
+# File>Import Dataset>From Excel...
+# Return to exercises and import those files
+
+
+
 
